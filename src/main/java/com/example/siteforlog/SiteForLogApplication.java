72c16065b2d1c9ffe437e986333bcab3cbc42d33
TheKelp1e <vova_krasnov_2004@mail.ru>
2022-11-15 22:41:02 +0300

    Initial commit

---
+++ src/main/java/com/example/siteforlog/SiteForLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.siteforlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteForLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteForLogApplication.class, args);
+	}
+
+}
